1ff23b85afba7cf9b618320a339942917a61cf22
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-01-21 10:16:13 +0530

    Create LowerAndUpperBound.java

---
+++ Searching_And_Sorting/Binary_Search/Codes/LowerAndUpperBound.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Main {
+     
+    public static int lowerBound(int[] arr, int target) {
+        
+        //we have to return the first occ if found else ceil
+        
+        int lo = 0;
+        int hi = arr.length -1;
+        int pa = arr.length;
+        
+        while(lo <= hi) {
+            if(arr[mid] >= target) {
+                pa = mid;
+                hi = mid -1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        
+        return pa;
+    } 
+     
+    public static int upperBound(int[] arr, int target) {
+        
+        // we have to find the ceil only
+        
+        int lo = 0;
+        int hi = arr.length-1;
+        
+        while(lo <= hi) {
+            
+            if(arr[mid] <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid -1;
+            }
+        }
+        
+        return lo;
+    }
+}
