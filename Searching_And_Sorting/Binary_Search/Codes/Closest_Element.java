2fd77b91443dd38969bb2e66b3829705ba1533f6
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-01-24 12:08:11 +0530

    Update Closest_Element.java

--- Searching_And_Sorting/Binary_Search/Codes/Closest_Element.java
+++ Searching_And_Sorting/Binary_Search/Codes/Closest_Element.java
@@ -1,16 +1,18 @@
-//This code is taken from heaters question so variables are wrt to that.
-
 public class Solution {
-   private int closest(int[] heaters, int house) {
-        int lb = lowerBound(heaters,house);
-        if(lb == heaters.length) return heaters[lb-1];
-        else if(lb == 0) return heaters[0];
+    public int closestElement(int[] arr, int target) {
+        int ceil = lowerBound(arr,target);
+        int floor = ceil-1;
         
-        else if(Math.abs(house-heaters[lb]) < Math.abs(house-heaters[lb-1])) {
-            return heaters[lb];
-        }
+        if(ceil == 0) return arr[ceil]; //floor does not exist
+        if(ceil == arr.length) return arr[floor]; //ceil does not exist
+    
+        if(Math.abs(target-arr[floor]) <= Math.abs(target-arr[ceil])) {
+            return arr[floor];
+        }  
         
-        else return heaters[lb-1];
+        return arr[ceil];
+        
     }
+    
 }
-}
+
