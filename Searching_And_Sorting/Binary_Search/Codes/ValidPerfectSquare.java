3104e1649b979a679783435e2f904f69e4070cab
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-01-25 09:48:09 +0530

    Create ValidPerfectSquare.java

---
+++ Searching_And_Sorting/Binary_Search/Codes/ValidPerfectSquare.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+       
+        if(num == 1) return true;
+        
+        int sqrt = mySqrt(num);
+        
+        if(sqrt*sqrt == num) return true;
+        
+        return false;
+    }
+    
+    public int mySqrt(int x) {
+       
+        if(x == 0) return 0;
+        long lo = 1;
+        long hi = x;
+        
+        long pa = 0;
+        
+        while(lo <= hi) {
+            long mid = lo + (hi-lo)/2;
+            
+            if(mid * mid == x) return (int)mid;
+            else if(mid * mid < x) {
+                pa = mid;
+                lo = mid + 1;
+            } else {
+                hi = mid -1;
+            }
+        }
+        
+        return(int) pa;
+    }
+}
