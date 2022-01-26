aeab660e434f3aad283d4c0b1e270e13c75c9299
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-01-26 12:08:58 +0530

    Create Count Rotation.java

---
+++ Searching_And_Sorting/Binary_Search/Codes/Count Rotation.java
@@ -0,0 +1,57 @@
+class Solution {
+    
+      private boolean isMin(int[] nums, int mid) {
+        int lVal = (mid - 1 > 0) ? nums[mid-1] : Integer.MAX_VALUE;
+        int rVal = (mid + 1 < nums.length) ? nums[mid + 1] : Integer.MAX_VALUE;
+        
+        if(nums[mid] < lVal && nums[mid] < rVal) return true;
+        
+        return false;
+    }
+    
+    private boolean isMax(int[] nums, int mid) {
+        int lVal = (mid - 1 > 0) ? nums[mid-1] : Integer.MIN_VALUE;
+        int rVal = (mid + 1 < nums.length) ? nums[mid + 1] : Integer.MIN_VALUE;
+        
+        if(nums[mid] > lVal && nums[mid] > rVal) return true;
+        
+        return false;
+    }
+    
+    public int findMin(int[] nums) {
+        
+        if(nums[0] < nums[nums.length-1]) return 0;
+        
+        int lo = 0;
+        int hi  = nums.length-1;
+        
+        while(lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            
+            if(isMin(nums,mid)) { //is minimum
+                return mid;
+            } 
+            
+            else if(isMax(nums,mid)) { //is maximum
+                return mid + 1; //this will be min
+            }
+            
+            else if(nums[lo] < nums[mid]) { //is left part sorted-> discard the left part
+                lo = mid + 1;
+            }
+            
+            else if(nums[mid] < nums[hi]) { //is right part sorted-> discard the right part
+                hi = mid - 1;
+            }
+        }
+        
+        return 0;
+    }
+    
+    int findKRotation(int arr[], int n) {
+        // code here
+        
+        int pivotIndex = findMin(arr);
+        return pivotIndex;
+    }
+}
