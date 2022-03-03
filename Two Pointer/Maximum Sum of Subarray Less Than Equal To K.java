1fab264c842dbe7330608719808bb69d06d5e00e
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-03-03 11:28:38 +0530

    Create Maximum Sum of Subarray Less Than Equal To K.java

---
+++ Two Pointer/Maximum Sum of Subarray Less Than Equal To K.java
@@ -0,0 +1,22 @@
+class Solution
+{
+    long findMaxSubarraySum(long arr[], int N,int X)
+    {
+        // Your code goes here
+        int left = 0;
+        long maxSum = 0, sum = 0;
+        
+        for(int right = 0;right<N;right ++) {
+            sum += arr[right];
+            
+            while(sum > X) {
+                sum -= arr[left];
+                left++;
+            } 
+            
+            maxSum = Math.max(maxSum,sum);
+        }
+        
+        return maxSum;
+    }
+}
