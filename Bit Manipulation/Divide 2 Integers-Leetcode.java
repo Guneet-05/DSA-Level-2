6501c954a5603f67bdd42b9d000020492e2235ae
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-05-03 13:42:41 +0530

    Update Divide 2 Integers-Leetcode.java

--- Bit Manipulation/Divide 2 Integers-Leetcode.java
+++ Bit Manipulation/Divide 2 Integers-Leetcode.java
@@ -1,4 +1,6 @@
 class Solution {
+//     This code passes all the test cases still gives TLE because we are subtracting the dividor each time 
+//     TC is O(dividend) in Worst Case
     public int divide(int dividend, int divisor) {
      
         if(dividend == 1 << 31 && divisor == -1) return Integer.MAX_VALUE;
