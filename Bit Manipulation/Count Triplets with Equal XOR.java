2bd087468aa2dbe435a00443c036d1a6d6b3da47
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-05-04 18:16:09 +0530

    Create Count Triplets with Equal XOR.java

---
+++ Bit Manipulation/Count Triplets with Equal XOR.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int countTriplets(int[] arr) {
+        int count = 0;
+        
+        for(int i=0;i<arr.length;i++) {
+            int xor = arr[i];
+            for(int k=i+1;k<arr.length;k++) {
+                xor = xor ^ arr[k];
+                if(xor == 0) {
+                    count += (k-i); //these are the number of triplets
+                }
+            }
+        }
+        
+        return count;
+    }
+}
