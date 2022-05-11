f80bffaa8634b5c6055454ebb99b6b8aae987b0d
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-05-11 21:25:33 +0530

    Create Rod Cutting.java

---
+++ Dynamic Programming/Rod Cutting.java
@@ -0,0 +1,23 @@
+class Solution{
+    
+    public int memo(int n,int[] price, int [] dp) {
+        if(n == 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        
+        int ans = 0;
+        
+        for(int cut=1;cut<=n;cut++) {
+            ans = Math.max(ans,price[cut -1] + memo(n-cut,price,dp));
+        }
+        
+        return dp[n] = ans;
+    }
+    
+    public int cutRod(int price[], int n) {
+        //code here
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return memo(n,price,dp);
+        
+    }
+}
