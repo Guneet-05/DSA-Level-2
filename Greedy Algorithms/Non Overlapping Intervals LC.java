1505d25469f59428237876263ebc2dc900f1d11a
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-05-18 17:39:50 +0530

    Create Non Overlapping Intervals LC.java

---
+++ Greedy Algorithms/Non Overlapping Intervals LC.java
@@ -0,0 +1,44 @@
+class Solution {
+    
+    public static class Interval {
+        int start;
+        int end;
+        
+        Interval() {}
+        
+        Interval(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+    }
+    
+    public static class MyComparator implements Comparator<Interval> {
+        public int compare(Interval obj1,Interval obj2) {
+            if(obj1.end == obj2.end) return obj1.start - obj2.start;
+            return obj1.end - obj2.end;
+        }
+    }
+    
+    public int eraseOverlapIntervals(int[][] mat) {
+        int n = mat.length;
+        Interval[] intervals = new Interval[n];
+        for(int i=0;i<n;i++) {
+            intervals[i] = new Interval(mat[i][0],mat[i][1]);
+        }
+        
+        Arrays.sort(intervals,new MyComparator());
+
+        //last interval's ending time
+        int limit = Integer.MIN_VALUE;
+        int count = 0; //count of non overlapping intervals
+
+        for(int i=0;i<intervals.length;i++) {
+            if(limit <= intervals[i].start) {
+                count++;
+                limit = intervals[i].end;
+            }
+        }
+
+        return n - count;   
+    }
+}
