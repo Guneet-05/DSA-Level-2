6496d888ab189012f08e4dfc088c841356bd676f
Guneet Malhotra <77784585+Guneet-05@users.noreply.github.com>
2022-06-08 15:50:34 +0530

    Create 13. Remove outermost parenthesis.java

---
+++ Stacks and Queues/Level 2/13. Remove outermost parenthesis.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Integer> stk = new Stack<>();
+        
+        StringBuilder res= new StringBuilder("");
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            
+            if(ch == '(') {
+                stk.push(i);
+            } else {
+                int j =stk.pop();
+                if(stk.size() == 0) {
+                    res.append(s.substring(j + 1,i));
+                }
+            }
+        }
+        
+        return res.toString();
+    }
+}
